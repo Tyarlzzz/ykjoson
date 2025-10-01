f01730de916b5452f836d041af7b0cdcf1f13254
Tyarlzzz <carpiocharles07@gmail.com>
2025-10-01 16:44:05 +0800

    database uploaad

---
+++ database/Database.php
@@ -0,0 +1,21 @@
+<?php
+    class Database {
+        private $host = 'localhost';
+        private $dbname = 'ykjoson'; 
+        private $username = 'root'; 
+        private $password = 'Happybirthdaytome07!'; 
+        private $conn;
+
+        public function getConnection() {
+            if ($this->conn == null) {
+                try {
+                    $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+                    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                } catch (PDOException $e) {
+                    die("Connection failed: " . $e->getMessage());
+                }
+            }
+            return $this->conn;
+        }
+    }
+?>
