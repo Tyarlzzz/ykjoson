5736990e61c9e1f4f906a61a50a8507178fd09f7
Eyowtf <josevaleowynlaurente@yahoo.com>
2025-10-05 13:55:50 +0800

    css fix, header compatibility (datatables & device)

--- layout/header.php
+++ layout/header.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    
     <title>PoS Laundry</title>
     <!-- CSS -->
     <link rel="stylesheet" href="../assets/css/styles.css">
@@ -12,6 +15,9 @@
 
     <!-- Tailwind -->
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- dataTables -->
+     <link rel="stylesheet" href="../assets/css/jquery.dataTables.min.css">
 
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
