c1365a7247381e27ff0883caf3e7d8875e04fac2
Nyxcode1 <erik.soliman@clsu2.edu.ph>
2025-10-16 22:05:49 +0800

    added imgs and pricing.php

--- Laundry/pricing.php
+++ Laundry/pricing.php
@@ -3,10 +3,22 @@
 <!-- Main Content Area -->
 <main class="font-[Switzer] flex-1 p-8 bg-gray-50 overflow-auto">
     <div class="w-full px-1">
-        <!-- Header -->
-        <div class="mb-8 flex justify-between items-center">
-            <h1 class="ps-3 text-3xl font-extrabold border-l-4 border-gray-900 text-gray-800">Point of Sale System</h1>
+        <div class="mb-6 flex justify-between items-center">
+            <h1 class="ps-3 text-3xl font-extrabold border-l-4 border-gray-900 text-gray-800">Inventory & Sales Report</h1>
             <p class="text-gray-500 text-base"><?php echo date('F j, Y'); ?></p>
+        </div>
+
+        <div class="flex mb-0">
+            <a href="expenses.php"
+            class="px-8 py-1 bg-gray-300 text-gray-700 font-semibold rounded-t-2xl z-0">Expenses</a>
+            <a href="salesReport.php" class="px-8 py-1 bg-gray-300 text-gray-700 font-semibold border-l-2 border-gray-400 rounded-t-2xl -ml-3 z-0">Sales
+            Report</a>
+            <a href="pricing.php"
+            class="px-8 py-1 bg-blue-600 text-white font-semibold border-l-2 border-gray-400 rounded-t-2xl -ml-3 z-0">Pricing</a>
+            <a href="manageRiders.php"
+            class="px-10 py-1 bg-gray-300 text-gray-700 font-semibold border-l-2 border-gray-400 rounded-t-2xl -ml-3 z-0">Riders</a>
+            <a href="archived.php"
+            class="px-8 py-1 bg-gray-300 text-gray-700 font-semibold border-l-2 border-gray-400 rounded-t-2xl -ml-3 z-10">Archived</a>
         </div>
 
         <div class="w-full">
