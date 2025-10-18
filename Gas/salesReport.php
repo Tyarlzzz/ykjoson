396dbb368365caf8796c683b2bfac8ec6ee2c1d0
jzsupsup-clsu <jaztinzuriel.supsup@clsu2.edu.ph>
2025-10-18 22:43:53 +0800

    ayos tabs ehgsenfqfqf

--- Gas/salesReport.php
+++ Gas/salesReport.php
@@ -9,16 +9,16 @@
 
     <!-- Navigation Tabs -->
     <div class="flex mb-0">
-      <a href="updateInventory.php"
-        class="px-8 py-1 bg-gray-300 text-gray-700 font-semibold rounded-t-2xl z-0">Inventory</a>
-      <a href="salesReport.php" class="px-5 py-1 bg-red-600 text-white font-semibold rounded-t-2xl -ml-3 z-0">Sales
+      <a href="expenses.php"
+        class="px-8 py-1 bg-gray-300 text-gray-700 font-semibold border-l-2 border-gray-400 rounded-t-2xl">Expenses</a>
+      <a href="salesReport.php" class="px-5 py-1 bg-red-600 text-white font-base rounded-t-2xl -ml-3 z-0">Sales
         Report</a>
+      <a href="updateInventory.php"
+        class="px-8 py-1 bg-gray-300 text-gray-700 font-semibold border-l-2 border-gray-400 rounded-t-2xl -ml-3 z-0">Inventory</a>
       <a href="../Rider/manageRiders.php"
         class="px-10 py-1 bg-gray-300 text-gray-700 font-semibold border-l-2 border-gray-400 rounded-t-2xl -ml-3 z-0">Riders</a>
       <a href="archived.php"
         class="px-8 py-1 bg-gray-300 text-gray-700 font-semibold border-l-2 border-gray-400 rounded-t-2xl -ml-3 z-10">Archived</a>
-      <a href="expenses.php"
-        class="px-8 py-1 bg-gray-300 text-gray-700 font-semibold border-l-2 border-gray-400 rounded-t-2xl -ml-3 z-10">Expenses</a>
     </div>
 
     <!-- Container -->
