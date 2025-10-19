8ba3e02869ab244f9b2d0e616ada2be7aac43ec9
jzsupsup-clsu <jaztinzuriel.supsup@clsu2.edu.ph>
2025-10-19 23:52:01 +0800

    working auth for inventory

---
+++ Gas/generateHash.php
@@ -0,0 +1,10 @@
+<?php
+// Change this to your desired password
+$password = "halimawmagpagawangsystem123";
+
+// Generate hash using bcrypt
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+// Display the hash
+echo "Your hashed password is: " . $hash;
+?>
