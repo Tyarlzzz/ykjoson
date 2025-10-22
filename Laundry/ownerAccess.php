17650558f7d30210735780b7c06e7451de6078c2
jaztin <j@gmail.com>
2025-10-22 15:26:11 +0800

    fix

--- Laundry/ownerAccess.php
+++ Laundry/ownerAccess.php
@@ -44,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </label>
         <div class="relative">
           <input type="password" id="password" name="password"
-            class="w-full px-4 py-2 ms-1 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none"
+            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none"
             placeholder="Enter password" required>
           <button type="button" id="togglePassword"
             class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700">
@@ -60,9 +60,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </svg>
           </button>
         </div>
-        <div class="m-0">
+        <div class="ms-1">
           <button type="button" id="forgotPassword"
-            class="text-sm font-medium mt-0">
+            class="text-sm font-medium">
             Forgot Password?
           </button>
         </div>
